f0551efe34a697cf827c2985c084bd8efcca8ea9
Luc de Nonancourt <luc.de.nonancourt@gmail.com>
2017-11-20 14:52:46 +0100

    ajout bundle user

---
+++ src/LL/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LL\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('LLUserBundle:Default:index.html.twig');
+    }
+}
